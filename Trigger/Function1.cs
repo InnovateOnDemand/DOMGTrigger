898d28a262ebcedfa0739da8e6fd8476db317eb5
Andrés Salazar <c-eduardo.salazar@fwi.com>
2022-11-17 10:47:42 -0500

    time schedule restore

--- Trigger/Function1.cs
+++ Trigger/Function1.cs
@@ -23,7 +23,7 @@ namespace Trigger
           }
 
           [FunctionName("Function1")]
-          public static async Task Run([TimerTrigger("*/30 * * * * *")] TimerInfo myTimer, ILogger log)
+          public static async Task Run([TimerTrigger("0 0 15 * * *")] TimerInfo myTimer, ILogger log)
           {
                try
                {
